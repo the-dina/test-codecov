869f85bee939c70af84bee75f238837357fd58be
the-dina <contact.the.dina@gmail.com>
2024-01-28 19:43:07 +1100

    book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_27_051332) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_28_084138) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
